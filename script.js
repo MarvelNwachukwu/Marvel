35c2cc636fa10b3ba1da01dc5f54bad1d9e8aef2
MarvelNwachukwu <marvellousn561@gmail.com>
2020-05-18 20:08:29 +0100

    finished CSS, started JS

--- script.js
+++ script.js
@@ -1,19 +1,26 @@
-let greetingText = document.querySelector('span');
+function ChangeGreeting() {
 
-var TodayDate = new Date();
-var CurrentHour = TodayDate.getHours();
-console.log(CurrentHour);
+    let greetingText = document.querySelector('span');
+    var TodayDate = new Date();
+    var CurrentHour = TodayDate.getHours();
+    console.log(CurrentHour);
 
-function ChangeGreeting (){
-    if(CurrentHour > 0 && CurrentHour < 12){
+    if (CurrentHour > 0 && CurrentHour < 12) {
         greetingText.innerHTML = 'Good Morning'
-    }else if(CurrentHour > 12 && CurrentHour < 16){
+    } else if (CurrentHour > 12 && CurrentHour < 16) {
         greetingText.innerHTML = 'Good Afternoon'
-    }else if(CurrentHour > 16 && CurrentHour < 24){
+    } else if (CurrentHour > 16 && CurrentHour < 24) {
         greetingText.innerHTML = 'Good Evening'
-    }else{
+    } else {
         greetingText.innerHTML = 'Hello there'
     }
 }
 
-ChangeGreeting()
+ChangeGreeting();
+
+
+    const MenuButton = document.querySelector('.MenuIcon');
+    const Menu = document.querySelector('#Menu');
+    const Content = document.querySelector('.Content')
+
+    MenuButton.addEventListener('click', removeAttribute(hidden));
